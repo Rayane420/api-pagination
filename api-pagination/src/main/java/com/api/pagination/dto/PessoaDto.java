87772c24bfdf8f67894e25a792c27515468cf1ff
Rayane Maciel <46855211+Rayane420@users.noreply.github.com>
2025-05-19 12:53:34 -0300

    feat:Adicionando DTO Pessoa

--- api-pagination/src/main/java/com/api/pagination/dto/PessoaDto.java
+++ api-pagination/src/main/java/com/api/pagination/dto/PessoaDto.java
@@ -1,5 +1,12 @@
 package com.api.pagination.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaDto {
 
     private String nome;
--- api-pagination/src/main/java/com/api/pagination/resource/PessoaResource.java
+++ api-pagination/src/main/java/com/api/pagination/resource/PessoaResource.java
@@ -1,6 +1,6 @@
 package com.api.pagination.resource;
 
-import com.api.pagination.model.Pessoa;
+import com.api.pagination.dto.PessoaDto;
 import com.api.pagination.service.PessoaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,8 +16,8 @@ public class PessoaResource {
     private final PessoaService pessoaService;
 
     @GetMapping("/lista-todos")
-    public ResponseEntity<Page<Pessoa>> buscarPessoas(@RequestParam int pagina,
-                                                      @RequestParam int itens){
+    public ResponseEntity<Page<PessoaDto>> buscarPessoas(@RequestParam int pagina,
+                                                         @RequestParam int itens){
         return ResponseEntity.ok(pessoaService.findAll(pagina, itens));
     }
 }
